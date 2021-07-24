d1b90c7295d4bb905b42979436d8a750bb294e19
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-24 10:37:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/Trolldcompany")
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/DKBOTZ")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/n_a_c_bot_developers")
+                            "⭕️ SUPPORT ⭕️", url="https://t.me/DK_BOTZ")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/nikhileashy/dcfilter")
+                            "YOU WANT OWN THIS BOT CONTACT", url="https://t.me/DKBOTZHELP")
                     ]
                 ]
             ),
